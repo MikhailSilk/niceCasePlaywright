bd9d85e1db03751ecd2303571c17065f5728bdb2
MikhailSilk <19680cm@gmail.com>
2025-06-30 15:37:28 +0400

    fix test

--- tests/main-page/uat/main-page.spec.ts
+++ tests/main-page/uat/main-page.spec.ts
@@ -1,5 +1,6 @@
 import test, { expect } from "@playwright/test";
 import { MainPage, MainPageBlocks } from "../../../src/page-object/main-page";
+import { log } from "console";
 
 test(`Trade-in block`, async ({ page }) => {
     await page.goto('https://nice-case.ru/', { waitUntil: "domcontentloaded" });
@@ -30,11 +31,23 @@ test(`Open fourth ads in main banner and click on the button "Выбрать в 
 test (`Open MacBook filter in "Лучшие предложения" block `, async ({page}) => {
     await page.goto('https://nice-case.ru/', { waitUntil: "domcontentloaded" });
     const mainPage = new MainPage(page);
+    const blockLocator =  mainPage.contentContainerLocator.locator(`.CUSTOM_CATALOG_2`);
     await mainPage.clickItemInBlock(`:text-is("Apple Watch")`, MainPageBlocks.CUSTOM_CATALOG_2);
-    await mainPage.
-    await expect(page).toHaveTitle("Ноутбук Apple MacBook")
-    console.log("test");
+    await expect(blockLocator).toContainText("Apple Watch series 10 46 мм корпус из алюминия цвета «чёрный глянец» спортивный ремешок черного цвета");
 })
+test (`Block load image `, async ({page}) => {
+    await page.goto('https://nice-case.ru/', { waitUntil: "domcontentloaded" });
+    // await page.route('**/kgg1de4wknhyw8qnot2d0vj4mtysy6uv.png', route => route.abort());
+    
+    const mainPage = new MainPage(page);
+    const blockLocator =  mainPage.contentContainerLocator.locator(`.${MainPageBlocks.CATALOG_SECTIONS}`);
+    await mainPage.navigateToBlock(MainPageBlocks.CATALOG_SECTIONS);
+    const imageLocator = blockLocator.getByAltText(`Apple на прозрачном фоне в магазине Nice case`);
+    const isLoaded = await imageLocator.evaluate((img: HTMLImageElement) =>
+        img.complete && img.naturalWidth > 0
+      );(
+    expect(isLoaded).toBeTruthy());
+}) 
 
 test (`Test`, async ({page}) => {
 }) 
