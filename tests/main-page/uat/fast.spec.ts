12a5e2392900bc72bb6ad81fc2872171342d8dd1
MikhailSilk <19680cm@gmail.com>
2025-07-06 15:19:20 +0400

    another try

--- tests/main-page/uat/fast.spec.ts
+++ tests/main-page/uat/fast.spec.ts
@@ -2,7 +2,7 @@ import test, { expect } from "@playwright/test";
 import { MainPage, MainPageBlocks } from "../../../src/page-object/main-page";
 import { log } from "console";
 
-test(`Repair block`, async ({ page }) => {
+test(`Repair block12`, async ({ page }) => {
     await page.goto('https://nice-case.ru/', { waitUntil: "domcontentloaded" });
     const mainPage = new MainPage(page);
     await mainPage.clickItemInBlock(`//*[@title = "Ремонт"]`, MainPageBlocks.BIG_BANNER_INDEX);
